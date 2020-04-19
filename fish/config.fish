a1bc5e1578b3d84a662d3f8da2237d470210d853
murt <murt@unpossible.io>
2020-04-19 20:47:15 +1000

    Fish settings no longer avoid vi keys, the ability to have !! as a macro just doesn't work consistently enough

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 
+# Vi Mode (this breaks keybindings for things like !!)
+fish_vi_key_bindings
+
 # Fisher (Automatic Install)
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME $HOME/.config
@@ -10,3 +13,4 @@ end
 
 # Prompt
 set -g pure_symbol_prompt "λ"
+set -g pure_symbol_reverse_prompt "𝑉"
