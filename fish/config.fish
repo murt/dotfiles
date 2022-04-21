a96949fdc7586727addc95644d059fddcde49254
murt <murt@unpossible.io>
2022-04-21 16:57:15 +1000

    Fixed up fishshell to use a better version of the bang-bang plugin that works with vi keybindings. Added asdf, rustup, and starship config to the shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+# No greeting
+set fish_greeting
+
 # XDG Spec
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
@@ -14,6 +17,12 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+# ASDF
+source $HOME/.asdf/asdf.fish
+
+# Rustup
+set -Ua fish_user_paths $HOME/.cargo/bin
 
 # Prompt
 set -gx STARSHIP_CONFIG $XDG_CONFIG_HOME/fish/starship.toml
